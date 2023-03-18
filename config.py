7ae493737506a4c304fa9a46827a2e4dbfd5dae2
Khalid <93127443+KhalidAlnujaidi@users.noreply.github.com>
2023-03-18 10:40:55 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,3 @@
-# OPENAI_API_KEY = "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"
-OPENAI_API_KEY = "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"
 
+OPENAI_API_KEY = "your api key here"
 
-#zqt_PMo0BHnmx1s8eTC7KmicljqtMQooNqag9fCVfg
